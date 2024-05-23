43ab1d63164525488c374cd1489a2eea5470c7c7
ulfbo <ulfbourelius71@gmail.com>
2024-05-23 15:12:13 +0200

    Mappings for TournamentGameDto

--- TournamentAPI/TournamentAPI.Data/Data/TournamentMappings.cs
+++ TournamentAPI/TournamentAPI.Data/Data/TournamentMappings.cs
@@ -9,6 +9,10 @@ public class TournamentMappings : Profile
     public TournamentMappings()
     {
         CreateMap<Tournament, TournamentDto>();
+        CreateMap<Tournament, TournamentDto>().ReverseMap();
         CreateMap<Game, GameDto>();
+        CreateMap<Game, GameDto>().ReverseMap();
+        CreateMap<Game, TournamentGameDto>();
+        CreateMap<Game, TournamentGameDto>().ReverseMap();
     }
 }
