6a2464e97008ee86bf22433128095ec8b844a201
ulfbo <ulfbourelius71@gmail.com>
2024-05-24 15:58:50 +0200

    Added DateFormatConverter and CustomDateContractResolver

---
+++ TournamentAPI/TournamentAPI/CustomDateContractResolver.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.Reflection;
+using Newtonsoft.Json.Serialization;
+
+namespace TournamentAPI;
+
+public class CustomDateContractResolver : Newtonsoft.Json.Serialization.DefaultContractResolver
+{
+    protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+    {
+        JsonProperty property = base.CreateProperty(member, memberSerialization);
+
+        if (property.PropertyType == typeof(DateTime) || property.PropertyType == typeof(DateTime?))
+        {
+            property.Converter = new DateFormatConverter(); // use the converter we created before
+        }
+
+        return property;
+    }
+}
